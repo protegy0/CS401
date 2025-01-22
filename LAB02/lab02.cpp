252f00d396ece89c297d5a400eebf032e9341d01
protegy0 <74997832+protegy0@users.noreply.github.com>
2025-01-22 20:20:35 +0000

    lab02

---
+++ LAB02/lab02.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+using namespace std;
+
+class ArrayStack {
+    public:
+        int size;
+        char* charArr;
+        int top;
+
+        ArrayStack(int sizeOfStack) {
+            charArr = new char[sizeOfStack];
+            size = sizeOfStack;
+            top = -1;
+        }
+
+        void push(char c) {
+            if (top == size - 1) {
+                cout << "Stack is full";
+            } else {
+                top++;
+                charArr[top] = c;
+            }
+        }
+
+        char pop() {
+            if (top == -1) {
+                cout << "Stack is empty. Returned zero.\n";
+                return '0';
+            } else {
+                char temp = charArr[top];
+                top--;
+                return temp;
+            }
+        }
+
+        bool isEmpty() {
+            return top == -1;
+        }
+
+};
+
+class Node {
+    public:
+        char value;
+        Node* next;
+
+        Node(char c) {
+            value = c;
+            next = nullptr;
+        }
+
+};
+
+class LinkedListStack {
+    public:
+        Node* head;
+        int size;
+
+        LinkedListStack() {
+            head = nullptr;
+            size = 0;
+        }
+
+        bool isEmpty() {
+            return size == 0;
+        }
+
+        void push(char c) {
+            Node newNode(c);
+            Node* node = &newNode;
+            if (head != nullptr) {
+                newNode.next = head;
+            }
+            head = node;
+            size++;
+        }
+
+        char pop() {
+            if (this->isEmpty()) {
+                cout << "Stack is empty. Returned zero.\n";
+                return '0';
+            }
+            Node* poppedNode = head;
+            head = head->next;
+            size--;
+            return poppedNode->value;
+        }
+};
+
+string balancedStringWithArray(string s) {
+    bool balanced = true;
+    ArrayStack stk(s.size());
+     
+    for (char c : s) {
+        if (c == '(' || c == '[' || c == '{') {
+            stk.push(c);
+        }
+        if (c == ')' || c == ']' || c == '}') {
+            if (stk.isEmpty()) {
+                balanced = false;
+                break;
+            }
+            char check = stk.pop();
+        }
+    }
+    if (stk.isEmpty()) {
+        return "This string is balanced.";
+    } else {
+        return "This string is unbalanced.";
+    }
+}
+string balancedStringWithLL(string s) {
+    bool balanced = true;
+    LinkedListStack stk;
+     
+    for (char c : s) {
+        if (c == '(' || c == '[' || c == '{') {
+            stk.push(c);
+        }
+        if (c == ')' || c == ']' || c == '}') {
+            if (stk.isEmpty()) {
+                balanced = false;
+                break;
+            }
+            char check = stk.pop();
+        }
+    }
+    if (stk.isEmpty()) {
+        return "This string is balanced.";
+    } else {
+        return "This string is unbalanced.";
+    }
+}
+
+
+
+int main() {
+
+    string s1 = "a + (b * [c - d])";
+    string s2 = "a + (b * [c - d)";
+    string s3 = "(";
+
+    cout << s1 << endl << "Array stack string parsing: " << balancedStringWithArray(s1) << endl << "Linked List stack string parsing: " << balancedStringWithLL(s1) << endl;
+    cout << s2 << endl << "Array stack string parsing: " << balancedStringWithArray(s2) << endl << "Linked List stack string parsing: " << balancedStringWithLL(s2) << endl;
+    cout << s3 << endl << "Array stack string parsing: " << balancedStringWithArray(s3) << endl << "Linked List stack string parsing: " << balancedStringWithLL(s3) << endl;
+
+
+    
+
+
+    return 0;
+}
